fec34522bdb252edd1431437e3f108ad3ae84190
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-04-21 10:42:43 -0500

    Update baumann-exercise-1.3.js

--- week-1/baumann-exercise-1.3.js
+++ week-1/baumann-exercise-1.3.js
@@ -16,7 +16,7 @@ Expected output:
   Manufacturer: Apple
   Model: iPhone XR
   Color: Yellow
-  Price: $99 
+  Price: 99.99
 */
 
 // lists details of formatted header, including first name, last name and assignment
@@ -26,34 +26,34 @@ console.log(header.display("Reva", "Baumann", "Exercise 1.3"));
 // Start Program
 
 // Declare an instance of the Cell Phone
-let myPhone = new CellPhone("Apple", "iPhone XR", "Yellow", "$99")
+let myPhone = new CellPhone("Apple", "iPhone XR", "Yellow", "99")
 
 // Display details of Cellphone
 function CellPhone(manufacturer, model, color, price)
 {
-    this.manufacturer = Manufacturer;
+    this.manufacturer = manufacturer;
     this.model = model;
     this.color = color;
     this.price = price;
-}
-/* Return the function of price */
-this.getPrice = function ()
-{
-    return this.price;
+
+    // get price
+    this.getPrice = function()
+    {
+        return this.price;
+    }
+
+    this.getModel = function()
+    {
+        return this.model;
+    }
+
+    this.getDetails = function()
+    {
+        return "Manufacturer: " + this.manufacturer + "\nModel: " + this.getModel() +
+        "\nColor: " + this.color + "\nPrice: " + this.getPrice(); 
+    }
 }
 
-/* Return the model of the cellphone */
-this.getModel = function ()
-{
-    return this.model;
-}
-
-/* Return Manufacturer */
-this.getDetails = function ()
-{
-    return "Manufacturer: " + this.manufacturer + "\nModel: " + this.getModel() + "\nColor: " + this.color + "\nPrice: " + this.getPrice();
-}
-}
 
 // Show details of cellphone information
 var cellPhone = new CellPhone("Apple", "iPhone XR", "Yellow", "99.99");
